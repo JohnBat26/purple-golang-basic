a61bbf7172880e3b8fc0ad47f7d7fc9f4fb41cd8
Eugene Batogov <e.batogov@cti.ru>
2025-07-20 16:17:30 +0300

    Исправления 3

--- main.go
+++ main.go
@@ -6,23 +6,28 @@ import (
 
 func main() {
 	fmt.Println("__ Конвертер валют __")
+	var usdToEur float64 = 0.85
+	var usdToRub float64 = 78.5
 
-	usdToRub, usdToEur := getUserInput()
+	// usdToRub, usdToEur := getUserInput()
 	eurToRub := usdToRub / usdToEur
 
 	fmt.Println("Euro to Ruble: ", eurToRub)
 }
 
-func getUserInput() (float64, float64) {
-	var usdToEur float64 = 0.85
-	var usdToRub float64 = 78.5
+func getUserInput() (float64, float64, float64) {
+	var amountMoney float64
+	var fromCurrency float64
+	var toCurrency float64
 
-	fmt.Print("Введите коэффициент преобразования доллара в евро: ")
-	fmt.Scan(&usdToEur)
-	fmt.Print("Введите коэффициент преобразования долларав рубль: ")
-	fmt.Scan(&usdToRub)
+	fmt.Print("Введите количество денег: ")
+	fmt.Scan(&amountMoney)
+	fmt.Print("Введите имя исходной валюты, например: usd, eur или rub: ")
+	fmt.Scan(&fromCurrency)
+	fmt.Print("Введите имя целевой валюты, например: usd, eur или rub: ")
+	fmt.Scan(&toCurrency)
 
-	return usdToEur, usdToRub
+	return amountMoney, fromCurrency, toCurrency
 }
 
 func calculate(amount float64, fromCurrency string, toCurrency string) {
