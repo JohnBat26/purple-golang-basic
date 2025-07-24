589d3773d73a07ed00fe00e5b71491586e4704a7
Eugene Batogov <e.batogov@cti.ru>
2025-07-24 22:04:12 +0300

    homework 10.9

--- 3-bin/main.go
+++ 3-bin/main.go
@@ -1,43 +1,17 @@
 package main
 
 import (
+	"demo/3-bin/bins"
 	"fmt"
-	"time"
 )
 
-type Bin struct {
-	id        string
-	name      string
-	private   bool
-	createdAt time.Time
-}
-
-type BinList struct {
-	bins [](Bin)
-}
-
-func NewBin(id, name string, private bool) Bin {
-	return Bin{
-		id:        id,
-		name:      name,
-		private:   private,
-		createdAt: time.Now(),
-	}
-}
-
-func NewBinList(bins [](Bin)) BinList {
-	return BinList{
-		bins,
-	}
-}
-
 func main() {
-	bin1 := NewBin("1", "bin1", false)
-	bin2 := NewBin("2", "bin2", true)
+	bin1 := bins.NewBin("1", "bin1", false)
+	bin2 := bins.NewBin("2", "bin2", true)
 
-	bins := []Bin{bin1, bin2}
+	bins_list := []bins.Bin{bin1, bin2}
 
-	binList := NewBinList(bins)
+	binList := bins.NewBinList(bins_list)
 
 	fmt.Println(binList)
 
