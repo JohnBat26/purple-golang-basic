ed8f70e6b0f426c85942c683ee14ab90fac052df
Eugene <JohnBat26@gmail.com>
2025-07-24 14:22:56 +0300

    Homework (#3)
    * Homework 7.10
    
    * Исправление замечаний 1
    
    * Homework 8.8
    
    ---------
    
    Co-authored-by: Eugene Batogov <e.batogov@cti.ru>

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,64 +5,73 @@ import (
 	"strings"
 )
 
-const UsDollar = "usd"
-const Euro = "eur"
-const RussianRuble = "rub"
+const (
+	UsDollar     = "usd"
+	Euro         = "eur"
+	RussianRuble = "rub"
+)
+
+type exchangeRates map[string]float64
 
 func main() {
-	fmt.Println("__ Конвертер валют __")
+	currencies := exchangeRates{
+		"usd-eur": 0.85,
+		"eur-usd": 1 / 0.85,
+		"usd-rub": 78.5,
+		"rub-usd": 1 / 78.5,
+		"eur-rub": 92.3,
+		"rub-eur": 1 / 92.3,
+	}
 
-	var usdToEur = 0.85
-	var usdToRub = 78.5
-	var eurToRub = usdToRub / usdToEur
+	fmt.Println("__ Конвертер валют __")
 
 	for {
 		amountMoney, fromCurrency, toCurrency := getUserInput()
 
-		var targetAmountMoney float64
-
-		switch {
-		case fromCurrency == UsDollar && toCurrency == Euro:
-			targetAmountMoney = amountMoney * usdToEur
-		case fromCurrency == UsDollar && toCurrency == RussianRuble:
-			targetAmountMoney = amountMoney * usdToRub
-		case fromCurrency == Euro && toCurrency == UsDollar:
-			targetAmountMoney = amountMoney / usdToEur
-		case fromCurrency == RussianRuble && toCurrency == UsDollar:
-			targetAmountMoney = amountMoney / usdToRub
-		case fromCurrency == Euro && toCurrency == RussianRuble:
-			targetAmountMoney = amountMoney * eurToRub
-		case fromCurrency == RussianRuble && toCurrency == Euro:
-			targetAmountMoney = amountMoney / eurToRub
-		default:
-			fmt.Println("Незнакомая комбинация валют")
-		}
+		targetAmountMoney := convert(&currencies, amountMoney, fromCurrency, toCurrency)
 
 		fmt.Println("Результаты конвертации:")
 		result := fmt.Sprintf("%0.2f %s => %.2f %s", amountMoney, fromCurrency, targetAmountMoney, toCurrency)
 		fmt.Println(result)
 
 		fmt.Println("Продолжить работу? y/n")
+
 		var answer string
+
 		fmt.Scan(&answer)
 
 		if strings.EqualFold(answer, "y") {
 			continue
-		} else {
-			break
 		}
+
+		break
 	}
+}
+
+func convert(currencies *exchangeRates, amountMoney float64, fromCurrency string, toCurrency string) float64 {
+	var targetAmountMoney float64
+
+	key := fromCurrency + "-" + toCurrency
+
+	if rate, exists := (*currencies)[key]; exists {
+		targetAmountMoney = amountMoney * rate
+	}
+
+	return targetAmountMoney
 }
 
 func getUserInput() (float64, string, string) {
 	var amountMoney float64
+
 	var fromCurrency string
+
 	var toCurrency string
 
 	for {
 		message := fmt.Sprintf("Введите имя исходной валюты, например: %s, %s или %s: ", UsDollar, Euro, RussianRuble)
 		fmt.Println(message)
 		fmt.Scan(&fromCurrency)
+
 		if fromCurrency != UsDollar && fromCurrency != Euro && fromCurrency != RussianRuble {
 			message = fmt.Sprintf("Вы должны ввести одно из трех значений: %s, %s или %s, Попробуйте снова.", UsDollar, Euro, RussianRuble)
 			fmt.Println(message)
@@ -74,10 +83,12 @@ func getUserInput() (float64, string, string) {
 	for {
 		fmt.Print("Введите сумму средств для конвертации: ")
 		_, err := fmt.Scan(&amountMoney)
+
 		if err != nil {
 			fmt.Println("Некорректное число! ", err)
 			continue
 		}
+
 		break
 	}
 
