09cf51f84beeead7af0eda6f7d545a1d66d1ab78
Eugene Batogov <e.batogov@cti.ru>
2025-07-30 18:55:24 +0300

    Homework 14.9

--- 3-bin/main.go
+++ 3-bin/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"demo/3-bin/bins"
 	"fmt"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
 	bin1 := bins.NewBin("1", "bin1", false)
 	bin2 := bins.NewBin("2", "bin2", true)
 
