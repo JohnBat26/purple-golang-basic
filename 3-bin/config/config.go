c24e428fd331ac5896f3f114a6dec5432a3b5bcd
Eugene <JohnBat26@gmail.com>
2025-08-04 10:24:47 +0300

    4 interfaces (#5)
    * Homework 12.15
    
    * Homework 12.15 2
    
    * Homework 14.9
    
    ---------
    
    Co-authored-by: Eugene Batogov <e.batogov@cti.ru>

---
+++ 3-bin/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	Key string
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Key: os.Getenv("KEY"),
+	}
+}
--- 3-bin/main.go
+++ 3-bin/main.go
@@ -3,15 +3,23 @@ package main
 import (
 	"demo/3-bin/bins"
 	"fmt"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
 	bin1 := bins.NewBin("1", "bin1", false)
 	bin2 := bins.NewBin("2", "bin2", true)
 
-	bins_list := []bins.Bin{bin1, bin2}
+	binsList := []bins.Bin{bin1, bin2}
 
-	binList := bins.NewBinList(bins_list)
+	binList2 := bins.NewBinList(binsList)
 
-	fmt.Println(binList)
+	fmt.Println(binList2)
 }
